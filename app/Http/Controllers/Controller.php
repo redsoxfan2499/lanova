b2911da457795da8eeb8d0f040bc023a65c91daf
Darren Ladner <dxladner@protonmail.com>
2022-04-15 14:37:51 -0500

    new test two file changes

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,5 +9,6 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    // test two comments
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
